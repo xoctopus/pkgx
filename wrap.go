a1a5f4359f8a4059cb031f632833e6194102bc83
saito <birdyfj@gmail.com>
2025-11-14 12:07:24 +0900

    feat: add identifer wrapper for ast parsing

---
+++ wrap.go
@@ -0,0 +1,91 @@
+package pkgx
+
+import (
+	"strings"
+
+	"github.com/xoctopus/x/syncx"
+)
+
+const (
+	prefix     = "xwrap_"
+	underscore = "_u_"
+	dot        = "_d_"
+	slash      = "_s_"
+)
+
+func NewWrapper() *Wrapper {
+	return &Wrapper{
+		p2w: syncx.NewSmap[string, string](),
+		w2p: syncx.NewSmap[string, string](),
+	}
+}
+
+// Wrapper provides bidirectional mappings between original and wrapped package
+// path. To make an identifier with full path can parsed by ast.
+// eg:
+//
+// path/to/pkg.TypeName => path_s_to_s_pkg_d_TypeName
+// p2w stores original → wrapped mappings.
+// w2p stores wrapped → original mappings.
+type Wrapper struct {
+	p2w syncx.Map[string, string]
+	w2p syncx.Map[string, string]
+}
+
+func (w *Wrapper) Clear() {
+	w.p2w.Clear()
+	w.w2p.Clear()
+}
+
+func (w *Wrapper) Unwrap(x string) string {
+	if v, ok := w.w2p.Load(x); ok {
+		return v
+	}
+	if _, ok := w.p2w.Load(x); ok {
+		return x
+	}
+
+	if strings.Contains(x, ".") || strings.Contains(x, "/") {
+		return x
+	}
+
+	p := x
+	p = strings.TrimPrefix(p, prefix)
+	p = strings.ReplaceAll(p, slash, "/")
+	p = strings.ReplaceAll(p, dot, ".")
+	p = strings.ReplaceAll(p, underscore, "_")
+
+	if !strings.Contains(p, ".") && !strings.Contains(p, "/") {
+		x = p
+	}
+
+	w.p2w.Store(p, x)
+	w.w2p.Store(x, p)
+
+	return p
+}
+
+func (w *Wrapper) Wrap(p string) string {
+	if x, ok := w.p2w.Load(p); ok {
+		return x
+	}
+
+	if strings.HasPrefix(p, prefix) {
+		return p
+	}
+
+	if !strings.Contains(p, ".") && !strings.Contains(p, "/") {
+		return p
+	}
+
+	x := p
+	x = strings.ReplaceAll(x, "_", underscore)
+	x = strings.ReplaceAll(x, ".", dot)
+	x = strings.ReplaceAll(x, "/", slash)
+	x = prefix + x
+
+	w.p2w.Store(p, x)
+	w.w2p.Store(x, p)
+
+	return x
+}
---
+++ wrap_test.go
@@ -0,0 +1,40 @@
+package pkgx_test
+
+import (
+	"testing"
+
+	. "github.com/xoctopus/x/testx"
+
+	"github.com/xoctopus/pkgx"
+)
+
+func TestWrapAndUnwrap(t *testing.T) {
+	cases := [][2]string{
+		{"net", "net"},
+		{"fmt", "fmt"},
+		{"encoding/json", "xwrap_encoding_s_json"},
+		{"github.com/path/to/pkg.Type", "xwrap_github_d_com_s_path_s_to_s_pkg_d_Type"},
+		{"github.com/path/to/pkg_test.Type", "xwrap_github_d_com_s_path_s_to_s_pkg_u_test_d_Type"},
+	}
+	w := pkgx.NewWrapper()
+
+	w.Clear()
+	for _, c := range cases {
+		Expect(t, w.Wrap(c[0]), Equal(c[1]))
+	}
+
+	w.Clear()
+	for _, c := range cases {
+		Expect(t, w.Unwrap(c[1]), Equal(c[0]))
+	}
+
+	w.Clear()
+	Expect(t, w.Wrap("xwrap_net"), Equal("xwrap_net"))
+	Expect(t, w.Unwrap("xwrap_net"), Equal("net"))
+	Expect(t, w.Unwrap("xwrap_net"), Equal("net"))
+	Expect(t, w.Unwrap("net.Addr"), Equal("net.Addr"))
+	Expect(t, w.Wrap("net"), Equal("net"))
+	Expect(t, w.Unwrap("xwrap_io_d_Reader"), Equal("io.Reader"))
+	Expect(t, w.Unwrap("io.Reader"), Equal("io.Reader"))
+	Expect(t, w.Unwrap("xwrap_io_d_Reader"), Equal("io.Reader"))
+}
